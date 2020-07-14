8d6cbb6a43a442a485ea10191a15265202d7115a
ryantoddgarza <ryantoddgarza@gmail.com>
2020-07-13 23:35:01 -0700

    add prahar logic

---
+++ src/components/organisms/SynthCabinet/SynthCabinet.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as logic from '../../../constants/generativeLogic';
+
+const SynthCabinet = () => {
+  console.log(logic.prahar())
+
+  return (
+    <div id="synthCabinet"></div>
+  )
+}
+
+export default SynthCabinet;
