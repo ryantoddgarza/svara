c690bbec7832ce074a75aa2e9181f85d8b573ebf
ryantoddgarza <ryantoddgarza@gmail.com>
2020-07-24 10:51:47 -0700

    inject scripts/stylesheets into layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
-import { appendScript } from './utils/appendScript';
 // import { Route, Switch } from 'react-router-dom';
 // import * as routes from './constants/routes';
-import Player from './components/organisms/Player';
+import include from './utils/include';
+import Layout from './utils/Layout';
 import getSynthEngine from './utils/SynthEngine/synthEngine';
 
 class App extends Component {
   componentDidMount() {
-    appendScript('/a11y.js');
+    include.sheet('https://fonts.googleapis.com/icon?family=Material+Icons');
+    include.script('/a11y.js');
   }
 
   render() {
     getSynthEngine;
     return (
       <div className="app">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-        <Player />
+        <Layout>
+        </Layout>
       </div>
     )
   }
-}
+};
 
 export default App;
