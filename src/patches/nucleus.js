60fe8f81e4d5c4595b79551b7294aa5d2b636a6f
ryantoddgarza <ryantoddgarza@gmail.com>
2020-07-26 11:59:11 -0700

    generative prahar based on local time

--- src/patches/nucleus.js
+++ src/patches/nucleus.js
@@ -1,13 +1,25 @@
 import { noteNums } from '../constants/midi';
+import { prahar } from '../constants/raga';
+
+const availableRagas = prahar.getRagas();
+const selectedRaga = availableRagas[0]; // randomly gen
+
+const generate = {
+  tempo: function() {
+    return Math.round(Math.random() * 100);
+  },
+}
 
 export const nucleus = {
-  tempo: 90,
+  tempo: generate.tempo(),
   meter: 4,
   tonic: 62,
   tonicToFreq: function() { return noteNums[this.tonic] },
-  ragaName: 'miyan ki todi',
-  prahar: 1, // gen based on prahar
+  ragaName: selectedRaga.name,
+  prahar: selectedRaga.prahar,
 };
+
+console.log(nucleus.tempo)
 
 // temporary for testing
 // make into init random gen
