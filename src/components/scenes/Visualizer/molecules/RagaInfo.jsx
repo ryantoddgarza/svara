f244185656df499171b3e79ff35bd4f7fdbb495c
ryantoddgarza <ryantoddgarza@gmail.com>
2020-07-29 08:37:26 -0700

    build: svara indexes to abbreviation

--- src/components/scenes/Visualizer/molecules/RagaInfo.jsx
+++ src/components/scenes/Visualizer/molecules/RagaInfo.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { selectedRaga } from '../../../../patches/nucleus';
 
-const RagaInfo = () => (
-  <div className="visualizer__raga-info">
-    <div className="visualizer__raga-name">{ `raga ${selectedRaga.name}` }</div>
-    <div className="visualizer__raga-detail">{ `thaat · ${selectedRaga.thaat}` }</div>
-    <div className="visualizer__raga-detail">{ `vadi · ${selectedRaga.vadi}` }</div>
-    <div className="visualizer__raga-detail">{ `samvadi · ${selectedRaga.samvadi}` }</div>
-    <div className="visualizer__raga-detail">{ `prahar · ${selectedRaga.prahar}` }</div>
-  </div>
-);
+const RagaInfo = () => {
+  const svaraNames = ['sa', 're', 're', 'ga', 'ga', 'ma', 'ma', 'pa', 'dha', 'dha', 'ni', 'ni'];
+
+  const indexToSvara = (i) => {
+    const svara = svaraNames[i];
+
+    return svara;
+  };
+
+  return (
+    <div className="visualizer__raga-info">
+      <div className="visualizer__raga-name">{ `raga ${selectedRaga.name}` }</div>
+      <div className="visualizer__raga-detail">{ `thaat · ${selectedRaga.thaat}` }</div>
+      <div className="visualizer__raga-detail">{ `vadi · ${indexToSvara(selectedRaga.vadi)}` }</div>
+      <div className="visualizer__raga-detail">{ `samvadi · ${indexToSvara(selectedRaga.samvadi)}` }</div>
+      <div className="visualizer__raga-detail">{ `prahar · ${selectedRaga.prahar}` }</div>
+    </div>
+  );
+};
 
 export default RagaInfo;
