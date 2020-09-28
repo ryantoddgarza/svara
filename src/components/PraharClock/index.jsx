731a812b12d2a98a0966298284a3486aab3edc2d
ryantoddgarza <ryantoddgarza@gmail.com>
2020-09-28 11:22:44 -0700

    refactoring to functional component

--- src/components/PraharClock/index.jsx
+++ src/components/PraharClock/index.jsx
@@ -1,36 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import nucleus from '~/synth/modules/nucleus';
 
-class PraharClock extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date().toLocaleTimeString('it-IT'),
+const PraharClock = () => {
+  const getCurrentTime = () => new Date().toLocaleTimeString('it-IT');
+
+  const [clock, setclock] = useState(getCurrentTime);
+  let timer;
+  useEffect(() => {
+    timer = setInterval(() => {
+      setclock(getCurrentTime);
+    }, 1000);
+
+    return function cleanup() {
+      clearInterval(timer);
+      timer = null;
     };
-  }
+  });
 
-  componentDidMount() {
-    this.intervalID = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
-
-  tick() {
-    this.setState({
-      time: new Date().toLocaleTimeString('it-IT'),
-    });
-  }
-
-  render() {
-    return (
-      <div className="visualizer__prahar">
-        <div className="visualizer__prahar-numeral">{nucleus.raga.prahar}</div>
-        <div className="visualizer__prahar-clock">{this.state.time}</div>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="visualizer__prahar">
+      <div className="visualizer__prahar-numeral">{nucleus.raga.prahar}</div>
+      <div className="visualizer__prahar-clock">{clock}</div>
+    </div>
+  );
+};
 
 export default PraharClock;
