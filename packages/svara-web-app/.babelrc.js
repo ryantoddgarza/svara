04695ac1748db62e48c478158d90f92cb14a7aeb
ryantoddgarza <ryantoddgarza@gmail.com>
2021-02-12 20:00:48 -0600

    build: install and config babel...remove-prop-types

--- packages/svara-web-app/.babelrc.js
+++ packages/svara-web-app/.babelrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+  env: {
+    production: {
+      plugins: ['transform-react-remove-prop-types'],
+    },
+  },
 };
