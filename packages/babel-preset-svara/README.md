e4325e60b3c3d3aa404f130e8c4de17b59a9f1ea
ryantoddgarza <ryantoddgarza@gmail.com>
2021-02-13 11:54:52 -0600

    build(babel): add babel preset package

---
+++ packages/babel-preset-svara/README.md
@@ -0,0 +1,13 @@
+# @svara/babel-preset-svara
+
+[Babel](https://babeljs.io/) preset for project packages.
+
+## Usage
+
+Add a _.babelrc_ file with this preset to the package root.
+
+```json
+{
+  "presets": ["@svara/babel-preset-svara"]
+}
+```
--- packages/svara-web-app/.babelrc.js
+++ packages/svara-web-app/.babelrc.js
@@ -1,9 +1,3 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
-  env: {
-    production: {
-      plugins: ['transform-react-remove-prop-types'],
-    },
-  },
+  presets: ['@svara/babel-preset-svara'],
 };
