806d3b3fd6c718c910e7c6f54d56bc0a8d5d6ebd
ryantoddgarza <ryantoddgarza@gmail.com>
2021-03-27 12:04:20 -0500

    refactor(web-app): append page to component filenames

--- packages/svara-web-app/src/components/App/index.jsx
+++ packages/svara-web-app/src/components/App/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { LANDING, HOME, VISUALIZER } from '~/constants/routes';
-import Visualizer from '~/pages/Visualizer';
-import Home from '~/pages/Home';
+import VisualizerPage from '~/pages/VisualizerPage';
+import HomePage from '~/pages/HomePage';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route path={LANDING} exact component={Visualizer} />
-      <Route path={HOME} component={Home} />
-      <Route path={VISUALIZER} component={Visualizer} />
+      <Route path={LANDING} exact component={VisualizerPage} />
+      <Route path={HOME} component={HomePage} />
+      <Route path={VISUALIZER} component={VisualizerPage} />
     </Switch>
   </BrowserRouter>
 );
