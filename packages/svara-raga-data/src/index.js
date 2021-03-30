f5e90d8667f79cc003c80ecc270a3d8d3d387ace
ryantoddgarza <ryantoddgarza@gmail.com>
2021-03-30 17:35:17 -0500

    refactor(raga-data): extract methods

--- packages/svara-raga-data/src/index.js
+++ packages/svara-raga-data/src/index.js
@@ -1,16 +1,1 @@
-import { ragas } from './data';
-import { filterBy, hourToPrahar } from './utils';
-
-function getPrahar(val) {
-  return hourToPrahar(val);
-}
-
-function getRagas() {
-  return ragas;
-}
-
-function getRagasByPrahar(prahar = hourToPrahar()) {
-  return filterBy('prahar', prahar);
-}
-
-export { getPrahar, getRagas, getRagasByPrahar };
+export * from './methods';
---
+++ packages/svara-raga-data/src/methods/getPrahar.js
@@ -0,0 +1,7 @@
+import { hourToPrahar } from '../utils';
+
+function getPrahar(val) {
+  return hourToPrahar(val);
+}
+
+export default getPrahar;
