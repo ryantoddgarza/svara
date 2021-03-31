99ec9aa3ef897b4e3a96a79a80a6e9099117680c
ryantoddgarza <ryantoddgarza@gmail.com>
2021-03-31 00:15:57 -0500

    feat(kriya): add random array helper function

--- packages/svara-kriya/src/core/helpers/random.js
+++ packages/svara-kriya/src/core/helpers/random.js
@@ -2,10 +2,16 @@
 // https://www.npmjs.com/package/random-js#distributions
 import { Random } from 'random-js';
 
-// Instance
 const random = new Random();
 
-// Custom
 random.fraction = (divisor) => 1 / (random.integer(1, divisor) + 1);
+
+random.array = (opts = {}) => {
+  const length = opts.length || 0;
+  const min = opts.min || 0;
+  const max = opts.max || 1;
+
+  return Array.from({ length }, () => random.integer(min, max));
+};
 
 export default random;
