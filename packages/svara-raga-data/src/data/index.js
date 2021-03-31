385ce0551c8e814dd6ffc5f17cf139de353b22a2
ryantoddgarza <ryantoddgarza@gmail.com>
2021-03-31 15:27:06 -0500

    feat(raga-data): add thaat data

--- packages/svara-raga-data/src/data/index.js
+++ packages/svara-raga-data/src/data/index.js
@@ -1,2 +1,3 @@
-export { default as ragas } from './ragas.json';
 export { default as praharas } from './praharas.json';
+export { default as ragas } from './ragas.json';
+export { default as thaats } from './thaats.json';
