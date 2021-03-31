156a126cb30b6c570b81e994cf0df7ef3fc89260
ryantoddgarza <ryantoddgarza@gmail.com>
2021-03-31 17:13:15 -0500

    refactor(raga-data): convert praharas data object to array

--- packages/svara-raga-data/src/utils/hourToPrahar.js
+++ packages/svara-raga-data/src/utils/hourToPrahar.js
@@ -1,11 +1,7 @@
 import { praharas } from '../data';
 
 function hourToPrahar(hour = new Date().getHours()) {
-  function findInTables(tab, val) {
-    return Object.keys(tab).find((key) => tab[key].includes(val));
-  }
-
-  return parseFloat(findInTables(praharas, hour));
+  return praharas.findIndex((list) => list.includes(hour)) + 1;
 }
 
 export default hourToPrahar;
