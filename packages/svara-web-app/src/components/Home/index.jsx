7a6f8fc44b83656cbe744a5209cd7e0401cf9e0d
ryantoddgarza <ryantoddgarza@gmail.com>
2021-04-15 13:32:22 -0500

    refactor(web-app): destructure cms in home component

--- packages/svara-web-app/src/components/Home/index.jsx
+++ packages/svara-web-app/src/components/Home/index.jsx
@@ -4,22 +4,28 @@ import Glossary from '~/components/Glossary';
 import { home } from '~/cms';
 
 const Home = () => {
-  const { settings, content } = home;
+  const {
+    settings: {
+      general: { subtitle },
+    },
+    content: { features, cards, glossary },
+  } = home;
+
   const cardCols = [1, 2, 3];
 
   return (
     <div className="home">
       <section className="home__section home__section--light">
         <div className="container home__container home__article">
-          <h1>{settings.general.subtitle}</h1>
-          {content.features.map(({ heading, body }) => (
+          <h1>{subtitle}</h1>
+          {features.map(({ heading, body }) => (
             <Fragment key={`key__${heading}`}>
               <h2>{heading}</h2>
               {body}
             </Fragment>
           ))}
           <Flex box gap={16}>
-            {content.cards.map(({ heading, body }) => (
+            {cards.map(({ heading, body }) => (
               <Flex item cols={cardCols} key={`key__${heading}`}>
                 <h3>{heading}</h3>
                 {body}
@@ -30,7 +36,7 @@ const Home = () => {
       </section>
       <section className="home__section home__section--dark">
         <div className="home__container">
-          <Glossary entries={content.glossary} />
+          <Glossary entries={glossary} />
         </div>
       </section>
     </div>
