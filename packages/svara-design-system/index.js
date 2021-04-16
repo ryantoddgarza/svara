567dceccf6eeb80b117dc86206a3f4e9c170e3e9
ryantoddgarza <ryantoddgarza@gmail.com>
2021-04-16 14:29:14 -0500

    feat(design-system): export header and player heights

--- packages/svara-design-system/index.js
+++ packages/svara-design-system/index.js
@@ -1,4 +1,16 @@
 export default {
   breakpoints: ['320px', '640px', '1280px', '1600px'],
   space: [0, 4, 8, 16, 32, 64],
+  header: {
+    height: {
+      mobile: 64,
+      desktop: 64,
+    },
+  },
+  player: {
+    height: {
+      mobile: 64,
+      desktop: 96,
+    },
+  },
 };
