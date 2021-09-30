60037f19d5e39f09c06b7069c53794c41cb4eff8
ryantoddgarza <ryantoddgarza@gmail.com>
2021-09-30 14:04:34 -0500

    refactor: replace styled-component with css styles

--- packages/svara-web-app/src/components/Home/index.jsx
+++ packages/svara-web-app/src/components/Home/index.jsx
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import { Flex } from '@svara/ui';
 import Glossary from '~/components/Glossary';
 import { home } from '~/cms';
-import { Home as StyledHome, Container, Section } from './styled';
 
 const Home = () => {
   const {
@@ -15,32 +14,38 @@ const Home = () => {
   const cardCols = [1, 2, 3];
 
   return (
-    <StyledHome>
-      <Section className="home__article">
-        <Container>
-          <h1>{subtitle}</h1>
-          {features.map(({ heading, body }) => (
-            <Fragment key={`key__${heading}`}>
-              <h2>{heading}</h2>
-              {body}
-            </Fragment>
-          ))}
-          <Flex box gap={16}>
-            {cards.map(({ heading, body }) => (
-              <Flex item cols={cardCols} key={`key__${heading}`}>
-                <h3>{heading}</h3>
-                {body}
+    <div className="home">
+      <section className="section light">
+        <div className="layout">
+          <div className="content">
+            <div className="article">
+              <h1>{subtitle}</h1>
+              {features.map(({ heading, body }) => (
+                <Fragment key={`key__${heading}`}>
+                  <h2>{heading}</h2>
+                  {body}
+                </Fragment>
+              ))}
+              <Flex box gap={16}>
+                {cards.map(({ heading, body }) => (
+                  <Flex item cols={cardCols} key={`key__${heading}`}>
+                    <h3>{heading}</h3>
+                    {body}
+                  </Flex>
+                ))}
               </Flex>
-            ))}
-          </Flex>
-        </Container>
-      </Section>
-      <Section dark>
-        <Container>
-          <Glossary entries={glossary} />
-        </Container>
-      </Section>
-    </StyledHome>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section dark">
+        <div className="layout">
+          <div className="content">
+            <Glossary entries={glossary} />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
