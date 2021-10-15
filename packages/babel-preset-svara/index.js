27e36c66d0881419665eb48b98af0d8ced9d8c12
ryantoddgarza <ryantoddgarza@gmail.com>
2021-10-15 11:33:22 -0500

    refactor: remove redundant class-properties plugin

--- packages/babel-preset-svara/index.js
+++ packages/babel-preset-svara/index.js
@@ -1,7 +1,6 @@
 module.exports = function preset() {
   return {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties'],
     env: {
       production: {
         plugins: ['transform-react-remove-prop-types'],
