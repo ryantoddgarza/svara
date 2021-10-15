22747d03a4a5ab161ffcc154dcf93d9d43ed02eb
ryantoddgarza <ryantoddgarza@gmail.com>
2021-10-15 11:03:13 -0500

    refactor: remove deprecated theme provider

--- packages/svara-web-app/src/components/Layout/index.jsx
+++ packages/svara-web-app/src/components/Layout/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ThemeProvider } from 'styled-components';
-import theme from '@svara/design-system';
 import Header from '~/components/Header';
 import Player from '~/components/Player';
 
 const Layout = ({ children }) => (
-  <ThemeProvider theme={theme}>
+  <>
     <Header />
     <div className="layout__main">{children}</div>
     <Player />
-  </ThemeProvider>
+  </>
 );
 
 Layout.propTypes = {
