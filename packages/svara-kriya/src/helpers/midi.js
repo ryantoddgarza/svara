a8740f510745396988451252acd0b44bbfb84911
ryantoddgarza <ryantoddgarza@gmail.com>
2022-12-27 17:04:13 -0600

    feat: add midi.fromPitchClass method

--- packages/svara-kriya/src/helpers/midi.js
+++ packages/svara-kriya/src/helpers/midi.js
@@ -2,8 +2,32 @@ import frequency from './frequency';
 
 function MIDI() {}
 
+// Number -> Number
+// Returns an equivalent frequency value given a MIDI value.
 MIDI.prototype.toFreq = function toFrequency(n) {
-  return frequency.list[n];
+  const freq = frequency.list[n];
+  return freq;
+};
+
+// Array, Number [, Number ] -> Array
+// Returns an array of MIDI values given a pitch class set array, a MIDI tonic
+// value, and an optional octave count.
+MIDI.prototype.fromPitchClass = function fromPitchClass(
+  set,
+  tonic,
+  nOctaves = 1,
+) {
+  const midiVals = Array.from(set, (step) => tonic + step);
+
+  if (nOctaves > 1) {
+    for (let n = 1; n < nOctaves; n += 1) {
+      midiVals.forEach((note) => {
+        midiVals.push(note + 12 * n);
+      });
+    }
+  }
+
+  return midiVals;
 };
 
 const midi = new MIDI();
