1a7cba149c5388a5b3cbb241de09736a82249acc
ryantoddgarza <ryantoddgarza@gmail.com>
2023-01-06 15:54:31 -0600

    refactor: delete deprecated PitchClassSet

--- packages/svara-kriya/src/PitchClassSet/defaults.js
+++
@@ -1,7 +0,0 @@
-const defaults = {
-  tonic: 60,
-  scaleSteps: [0, 2, 4, 5, 7, 9, 11],
-  octaves: 1,
-};
-
-export default defaults;
--- packages/svara-kriya/src/index.js
+++ packages/svara-kriya/src/index.js
@@ -2,6 +2,5 @@ export * from './helpers';
 export * from './modules';
 export { default as Composer } from './Composer';
 export { default as Measure } from './Measure';
-export { default as PitchClassSet } from './PitchClassSet';
 export { default as Queue } from './Queue';
 export { default as Subdivision } from './Subdivision';
